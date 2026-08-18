d853700b23f13c60164ec2f14f97401dd15aaed3
Early Pajo <earlypajo@gmail.com>
2026-08-18 17:25:06 +0800

    Describe your changes

---
+++ agang.java
@@ -0,0 +1,10 @@
+public class agang {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 15;
+        int sum = num1 + num2;
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Sum: " + sum);
+    }
+}
---
+++ pajo.java
@@ -0,0 +1,15 @@
+public class pajo {
+
+    public static void main(String[] args) {
+
+        String studentName = "Juan Dela Cruz";
+        int age = 18;
+        String course = "BSIT";
+        int yearLevel = 1;
+
+        System.out.println("Name: " + studentName);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+        System.out.println("Year Level: " + yearLevel);
+    }
+}
